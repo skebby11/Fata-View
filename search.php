74231d132005cce566e1da5bd435c064e9db2be0
Sebastiano Riva <skebby11@gmail.com>
2019-07-16 12:57:28 +0200

    Fixed film search

--- search.php
+++ search.php
@@ -50,6 +50,7 @@ $keyword = $_POST["s"];
 	</div>
 
 	<form>
+	  <div class="container">
 		<h3 class="lasttext">Risultati della ricerca</h3>
 		<div class="allseriesrow">
 		<?php
@@ -58,26 +59,32 @@ $keyword = $_POST["s"];
 			if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; }; 
 			$start_from = ($page-1) * $results_per_page;
 				
-			$query = "SELECT id, nome, descr, poster FROM serie WHERE nome LIKE '%" . 
-           $keyword . "%'
-           UNION
-           SELECT id, titolo, descr, poster as type FROM film WHERE titolo LIKE '%" . 
-           $keyword . "%'  ORDER BY ID DESC LIMIT $start_from, ".$results_per_page;
+			$query1 = "SELECT id, nome, descr, poster FROM serie WHERE nome LIKE '%" . 
+           $keyword . "%' ORDER BY ID DESC LIMIT $start_from, ".$results_per_page;
+				
+			$query2 = " SELECT id, titolo, descr, poster FROM film WHERE titolo LIKE '%" . 
+           $keyword . "%' ORDER BY ID DESC LIMIT $start_from, ".$results_per_page;
 
 
 				
-		$result = mysqli_query($db,$query);
+		$result1 = mysqli_query($db,$query1);
+		$result2 = mysqli_query($db,$query2);
 		
-		if (mysqli_num_rows($result) > 0) {
-		while($row = mysqli_fetch_assoc($result)) {
+		if (mysqli_num_rows($result1) > 0) {
+		while($row = mysqli_fetch_assoc($result1)) {
 			echo "<div class='serierow'><a href='serie?id=". $row["id"] ."'><img src='" . $row["poster"] ."'><p><strong>" . $row["nome"] ."</strong><br><br>" . substr($row["descr"], 0, 500) ."...</p></a></div>";
+		}
+		}
+		elseif (mysqli_num_rows($result2) > 0) {
+		while($row = mysqli_fetch_assoc($result2)) {
+			echo "<div class='serierow'><a href='film?id=". $row["id"] ."'><img src='" . $row["poster"] ."'><p><strong>" . $row["titolo"] ."</strong><br><br>" . substr($row["descr"], 0, 500) ."...</p></a></div>";
 		}
 		} else {
 			echo "Nessun risultato trovato :(";
 		}
 		?>
 		</div>
-		
+	  </div>
 	</form>
 	
 	<div class="footer">
