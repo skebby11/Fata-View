fa408ec191e2f706d0901d5628dd8de4c6f4df31
skebby11 <skebby11@gmail.com>
2020-05-31 15:56:48 +0200

    Fixed login and register pages

--- login.php
+++ login.php
@@ -5,57 +5,19 @@ include('functions.php');
 $goto = $_GET['goto'];
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Fata Streaming - Login</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css?1.3.5.24.43">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
 
-<!--	<style>
-	@media (min-width: 992px) { 
-		.header {
-			max-width: 970px;
-		}
-		.menu {
-			max-width: 970px;
-		}
-		form {
-			max-width: 970px;
-		}
-	</style>-->
-</head>
+$pagetitle = "Login - Fata Streaming";
+
+include('inc/sections/head.php');
+?>
+
 <body>
+<?php
 
-	<div class="header">
-		<img src="assets/img/FATASlogo.png">
-	</div>
-		<div class="menu">
-		<ul>
- 		 <li><a href="/">Home</a></li>
- 		 <li><a href="lista-serie">Serie</a></li>
- 		 <li><a href="lista-film">Film</a></li>
-		</ul>
-		<!-- logged in user information -->
-		<div class="profile_info" align="right"> 
 
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
-
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="index.php?logout='1'" style="color: red;">logout</a>
-					</small>
-
-				<?php else : ?>
-				<ul class="login">
- 		 			<li class="login"><a class="active" href="login">Login</a></li>
-  		 			<li class="login"><a href="register">Signup</a></li>
-				</ul>
-				<?php endif ?>
-		</div>
-	</div>
+include('inc/sections/header.php');
+?>
 	
 	<form method="post" action="login.php">
 	  <div class="container">
@@ -83,5 +45,5 @@ $goto = $_GET['goto'];
 	</form>
 
 		</div>
-</body>
-</html>
+
+<?php include('inc/sections/footer.php') ?>
--- register.php
+++ register.php
@@ -1,49 +1,22 @@
 <?php include('functions.php');
 
 $goto = $_GET['goto']; ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Fata Streaming - Register</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css?1.3.5.24.43">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-<!--	<style>
-	@media (min-width: 992px) { 
-		.header {
-			max-width: 970px;
-		}
-		.menu {
-			max-width: 970px;
-		}
-		form {
-			max-width: 970px;
-		}
-	</style>-->
-	
-</head>
-<body>
-	<div class="header">
-		<img src="assets/img/FATASlogo.png">	
-	</div>
-	<div class="menu">
-		<ul>
- 		 <li><a href="/">Home</a></li>
- 		 <li><a href="lista-serie">Serie</a></li>
- 		 <li><a href="lista-film">Film</a></li>
-		</ul>
-		
-		<!-- logged in user information -->
-		<div class="profile_info" align="right"> 
 
-			<div>
-				<ul class="login">
- 		 		<li class="login"><a href="login">Login</a></li>
-  		 		<li class="login"><a class="active" href="register">Signup</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+
+<?php
+
+$pagetitle = "Registrati - Fata Streaming";
+
+include('inc/sections/head.php');
+?>
+
+
+<body>
+<?php
+
+
+include('inc/sections/header.php');
+?>
 	
 	<form method="post" action="register">
 	<div class="container">
@@ -76,5 +49,5 @@ $goto = $_GET['goto']; ?>
 		</p>
 		</div>
 	</form>
-</body>
-</html>
+	
+<?php include('inc/sections/footer.php') ?>
